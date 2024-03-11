c0659678514b3fa7e73f5432ee6c7a797302a010
Debnath Mahapatra <128223356+Debnath23@users.noreply.github.com>
2024-03-11 22:45:42 +0530

    Landing page 75% completed

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -5,7 +5,7 @@ import Arrow from "../Svg/Arrow";
 export default function HeroSection() {
   return (
     <div className="relative z-20 md:h-screen md:max-h-[950px] h-auto text-yellow-900">
-      <section className="mx-auto max-w-5xl px-6 pb-[90px] md:h-screen md:max-h-[950px] md:max-w-7xl">
+      <section className="mx-auto max-w-5xl px-6 md:h-screen md:max-h-[950px] md:max-w-7xl">
         <div className="flex h-full flex-col items-center justify-between md:flex-row md:pb-24">
           <div className="origin-center-left order-2 max-w-3xl animate-hero-text-slide-up-fade sm:shrink-0 md:order-1 lg:pl-16">
             <h1 className="font-book font-styling font-display font-effect-hero text-center md:text-left text-[4rem] md:text-7xl leading-[4.35rem] md:leading-[5rem] tracking-tight font-gradient">
@@ -13,9 +13,8 @@ export default function HeroSection() {
               <br />
               Community
             </h1>
-            <p className="sans mb-8 mt-4 max-w-[30rem] text-center leading-7 md:text-left text-base md:text-[1.125rem] md:leading-[1.5] text-slate-11 font-normal">
-              The best way to reach humans instead of spam folders. Deliver
-              transactional and marketing emails at scale.
+            <p className="sans mb-8 mt-4 max-w-[30rem] text-center leading-7 md:text-left text-base md:text-[1.125rem] md:leading-[1.5] text-slate-11 font-normal text-slate-700">
+            The best way to reach humans instead of spam folders. Deliver transactional and marketing emails at scale.
             </p>
             <div className="flex flex-col justify-center gap-4 md:flex-row md:justify-start">
               <a
@@ -27,9 +26,9 @@ export default function HeroSection() {
               </a>
               <a
                 className="text-base h-12 pl-5 pr-2 gap-0 font-semibold bg-slate-1 border-slate-1 text-slate-11 hover:bg-slate-5 hover:text-slate-12 focus-visible:ring-4 focus-visible:ring-slate-7 focus-visible:outline-none focus-visible:bg-slate-6 disabled:hover:bg-slate-1 inline-flex items-center border-2 justify-center select-none rounded-full disabled:cursor-not-allowed disabled:opacity-70 transition ease-in-out duration-200"
-                href="/docs"
+                href="/subjects"
               >
-                Documentation
+                Subjects
                 <Arrow />
               </a>
             </div>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,8 @@
 "use client";
-import React, { useState } from "react";
-import { HoveredLink, Menu, MenuItem } from "../ui/navbar-menu";
-import { cn } from "@/utils/cn";
+import React from "react";
 import Link from "next/link";
-import Arrow from "../Svg/Arrow";
 
-export default function Navbar({ className }: { className?: string }) {
-  const [active, setActive] = useState<string | null>(null);
+export default function Navbar() {
   return (
     <div className="mx-auto hidden h-[68px] w-full items-center justify-between transition duration-500 ease-in-out md:flex md:mt-8">
       <div className="flex lg:w-[225px]">
@@ -30,71 +26,28 @@ export default function Navbar({ className }: { className?: string }) {
         </span>
       </div>
 
-      <div
-        className={cn(
-          "fixed top-10 inset-x-0 max-w-2xl mx-auto z-50",
-          className
-        )}
-      >
-        <Menu setActive={setActive}>
-          <Link href={"/"}>
-            <MenuItem
-              setActive={setActive}
-              active={active}
-              item="Home"
-            ></MenuItem>
-          </Link>
-          <MenuItem setActive={setActive} active={active} item="Subjects">
-            <div className="flex flex-col space-y-4 text-sm">
-              <HoveredLink href="/compiler-design">Compiler Design</HoveredLink>
-              <HoveredLink href="/ai">Artificial Intelligence</HoveredLink>
-              <HoveredLink href="/computer-graphics">
-                Computer Graphics
-              </HoveredLink>
-            </div>
-          </MenuItem>
-          <MenuItem
-            setActive={setActive}
-            active={active}
-            item="Learn from AI tools"
-          >
-            <div className="flex flex-col space-y-4 text-sm">
-              <HoveredLink href="https://chat.openai.com/">ChatGPT</HoveredLink>
-              <HoveredLink href="https://gemini.google.com/">
-                Gemini
-              </HoveredLink>
-              <HoveredLink href="https://copilot.microsoft.com/">
-                Copilot
-              </HoveredLink>
-              <HoveredLink href="https://chat.olakrutrim.com/">
-                Krutrim
-              </HoveredLink>
-            </div>
-          </MenuItem>
-          <Link href={"/contact"}>
-            <MenuItem
-              setActive={setActive}
-              active={active}
-              item="Contact Us"
-            ></MenuItem>
-          </Link>
-          <Link href={"/support"}>
-            <MenuItem
-              setActive={setActive}
-              active={active}
-              item="Support Us"
-            ></MenuItem>
-          </Link>
-        </Menu>
+      <div className="md:w-[700px] md:h-[50px] border-2 text-lg h-12 pl-5 pr-5 mt-3 gap-4 bg-slate-100 text-slate-500 items-center justify-center rounded-full inline-flex sticky top-5">
+        <Link href={"/"}>
+          <p className="mr-4">Home</p>
+        </Link>
+        <Link href={"/subjects"}>
+          <p className="mr-4">Subjects</p>
+        </Link>
+        <Link href={"/contact"}>
+          <p className="mr-4">Contact Us</p>
+        </Link>
+        <Link href={"/support"}>
+          <p>Support Us</p>
+        </Link>
       </div>
 
-      <div className="flex gap-4 md:mr-[175px]">
-        <a
+      <div className="flex md:mr-[175px]">
+        <Link
           className="text-lg h-12 pl-5 pr-5 mt-3 gap-0 bg-slate-100 text-slate-500 hover:bg-slate-200 inline-flex items-center border-2 justify-center rounded-full"
           href="/signin"
         >
           Sign in
-        </a>
+        </Link>
       </div>
     </div>
   );
